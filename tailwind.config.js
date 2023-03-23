599bd4bf8169032d0b4c83b5eac53bbb3a0bdc48
setyawannn <prayogasetyawan.adi@gmail.com>
2023-03-23 20:44:17 +0700

    add: folder management and config assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F24F09",
+        secondary: "#FFC017",
+        dark: "#070E13",
+        "grey-text": "#7C7C7C",
+        "grey-line": "#CFCFCF",
+      },
+      fontFamily: {
+        gts: ["var(--font-gts)"],
+        poppins: ["var(--font-poppins)"],
+        ssp: ["var(--font-ssp)"],
+      }
+
+    },
   },
   plugins: [],
 }
