445a9e0cf91b43cfa5f68a0264faa2b3b0c1906b
unknown <zaskiakiaa802@gmail.com>
2023-03-27 07:39:25 +0700

    add trending topic

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Landing from "@/components/Landing";
 import SideBar from "@/components/Sidebar/SideBar";
 import CarousellNews from "@/components/mainNews";
+import { TrendingCard } from "@/components/User/TrendingCard";
 export default function page() {
   return (
     <div className="text-red-500">
       <Landing />
+      <TrendingCard />
       <CarousellNews />
       <SideBar />
     </div>
